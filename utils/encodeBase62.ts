e2c7004c87fbecb03137c297ee31e39005572db9
Daniel Adeabah <daniel.adeabah@amalitech.com>
2024-07-26 19:42:41 +0000

    refactor: base62 encoding functions

--- utils/encodeBase62.ts
+++ utils/encodeBase62.ts
@@ -1,24 +1,25 @@
 // Base62 characters
-const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const BASE62_CHARS =
+  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const BASE62_RADIX = BASE62_CHARS.length;
 
-export function encodeBase62(number: number) {
+function toBase62(number: number): string {
+  if (number === 0) return BASE62_CHARS[0];
+
   let encoded = "";
   while (number > 0) {
-    const remainder = number % 62;
-    encoded = chars[remainder] + encoded;
-    number = Math.floor(number / 62);
+    const remainder = number % BASE62_RADIX;
+    encoded = BASE62_CHARS[remainder] + encoded;
+    number = Math.floor(number / BASE62_RADIX);
   }
   return encoded;
 }
 
+export function encodeBase62(number: number): string {
+  return toBase62(number);
+}
+
 export function assigneesEncodeBase62(date: number, index: number): string {
-  const characters =
-    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let result = "";
-  let num = date + index;
-  while (num > 0) {
-    result = characters[num % 62] + result;
-    num = Math.floor(num / 62);
-  }
-  return result;
+  const num = date + index;
+  return toBase62(num);
 }
