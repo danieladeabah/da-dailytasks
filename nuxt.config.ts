95211df3beb99d034e1b4401a82f39787e9b9a22
danieladeabah <daniel.adeabah@amalitech.com>
2024-08-27 10:09:44 +0000

    remove unused snackbar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["nuxt-icon", "nuxt-snackbar", "@nuxt/ui", "@nuxtjs/color-mode"],
-  snackbar: {
-    top: true,
-    right: true,
-    duration: 5000,
-  },
+  modules: ["nuxt-icon", "@nuxt/ui", "@nuxtjs/color-mode"],
   postcss: {
     plugins: {
       tailwindcss: {},
