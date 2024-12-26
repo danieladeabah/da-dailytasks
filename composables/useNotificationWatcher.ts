60627df4f994fd1b9ae6f61563bd4af1494985c4
danieladeabah <daniel.adeabah@amalitech.com>
2024-12-26 22:26:22 +0000

    feat: refactor notification watcher to use a reusable function for handling notifications

--- composables/useNotificationWatcher.ts
+++ composables/useNotificationWatcher.ts
@@ -1,36 +1,33 @@
 import { useAuthenticationStore } from "@/store/auth";
 import { useTasksStore } from "@/store/tasks";
 
+type NotificationColor = "blue" | "red" | "green";
+
 export function useNotificationWatcher() {
   const AuthStore = useAuthenticationStore();
   const TaskStore = useTasksStore();
   const toast = useToast();
 
-  watch(
-    () => AuthStore.success || AuthStore.error,
-    (newToastNotification) => {
-      if (newToastNotification) {
-        toast.add({
-          title: newToastNotification,
-          timeout: 2000,
-          color: AuthStore.error ? "red" : "blue",
-        });
-        AuthStore.clearSuccessAfterDelay();
+  const watchNotifications = (
+    store: any,
+    colorMap: { success: NotificationColor; error: NotificationColor }
+  ) => {
+    watch(
+      () => store.success || store.error,
+      (newToastNotification) => {
+        if (newToastNotification) {
+          toast.add({
+            title: newToastNotification,
+            timeout: 2000,
+            color: store.error ? colorMap.error : colorMap.success,
+          });
+          store.clearSuccessAfterDelay();
+        }
       }
-    }
-  );
+    );
+  };
 
-  watch(
-    () => TaskStore.success || TaskStore.error,
-    (newToastNotification) => {
-      if (newToastNotification) {
-        toast.add({
-          title: newToastNotification,
-          timeout: 2000,
-          color: TaskStore.error ? "red" : "green",
-        });
-        TaskStore.clearSuccessAfterDelay();
-      }
-    }
-  );
+  // Watch notifications for AuthStore and TaskStore
+  watchNotifications(AuthStore, { success: "blue", error: "red" });
+  watchNotifications(TaskStore, { success: "green", error: "red" });
 }
