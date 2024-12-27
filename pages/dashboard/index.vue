387d9c0d6e499a107222cd6a70aa99674fc31442
danieladeabah <daniel.adeabah@amalitech.com>
2024-12-27 15:46:45 +0000

    feat: update login link to redirect to dashboard and add dashboard layout component

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <AppWrapper>
+    <NavigationHeader />
+    <ProjectActivityLayout />
+    <DashboardCreateTasks />
+    <NavigationFooter />
+  </AppWrapper>
+</template>
+
+<script setup lang="ts">
+import AppWrapper from "~/layouts/app-wrapper.vue";
+</script>
