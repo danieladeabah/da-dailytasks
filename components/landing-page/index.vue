19bfc6d5e87bdf73264dbd3601eb58ffc8ed1d45
danieladeabah <daniel.adeabah@amalitech.com>
2024-12-27 15:45:22 +0000

    feat: add landing page with navigation link to tasks page

---
+++ components/landing-page/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold text-center my-5">
+      <NuxtLink to="/dashboard">Go to Tasks Page</NuxtLink>
+    </h1>
+    <p class="text-center">This is the Landing Page</p>
+  </div>
+</template>
