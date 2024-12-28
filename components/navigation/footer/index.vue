8eb6205679565a8b822149de9e2d2e7a4b51b862
danieladeabah <daniel.adeabah@amalitech.com>
2024-12-28 15:05:10 +0000

    feat: add portfolio link to footer component

--- components/navigation/footer/index.vue
+++ components/navigation/footer/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="w-full text-center py-5">
     <div class="text-sm flex items-center justify-center gap-3">
-      © {{ currentYear }} <heartIcon /> Daniel
+      © {{ currentYear }} <heartIcon />
+      <a href="https://danieladeabah.vercel.app/" target="_blank"
+        >Daniel Adeabah's Portfolio</a
+      >
     </div>
   </div>
 </template>
