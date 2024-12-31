750b2a908c14d2fa5ffc311d334bef351b2fb4bf
danieladeabah <daniel.adeabah@amalitech.com>
2024-12-31 01:07:30 +0000

    fix: update footer link text for consistency and brevity

--- src/components/navigation/footer/index.vue
+++ src/components/navigation/footer/index.vue
@@ -2,9 +2,7 @@
   <div class="my-10 w-full py-5 text-center">
     <div class="flex items-center justify-center gap-3 text-sm">
       © {{ currentYear }} <heartIcon />
-      <a href="https://danieladeabah.vercel.app/" target="_blank"
-        >Daniel Adeabah's Portfolio</a
-      >
+      <a href="https://danieladeabah.vercel.app/" target="_blank">./adeabah</a>
     </div>
   </div>
 </template>
