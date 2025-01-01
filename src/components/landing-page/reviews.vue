e85b5bfa4f56457f55c326059affa528829942a8
danieladeabah <daniel.adeabah@amalitech.com>
2025-01-01 23:13:32 +0000

    feat: wrap user reviews section in UiKitsUiSlotsProjectActivitySlot for improved layout

--- src/components/landing-page/reviews.vue
+++ src/components/landing-page/reviews.vue
@@ -1,27 +1,29 @@
 <template>
-  <div class="container my-5 text-center">
-    <h2 class="mb-6 text-3xl font-bold text-gray-800">User Reviews</h2>
-    <p class="mb-2 text-gray-600">
-      What our users have to say about our task management solution.
-    </p>
-  </div>
-  <div class="grid grid-cols-1 gap-5 p-6 sm:grid-cols-2 lg:grid-cols-3">
-    <UAlert
-      v-for="(user, index) in user_reviews"
-      :key="index"
-      :title="user.name"
-      :description="user.description"
-      :avatar="{ src: user.userImage, alt: user.name }"
-    >
-      <template #avatar="{ avatar }">
-        <UAvatar
-          v-bind="avatar"
-          :chip-color="index % 2 === 0 ? 'green' : 'orange'"
-          chip-position="top-left"
-        />
-      </template>
-    </UAlert>
-  </div>
+  <UiKitsUiSlotsProjectActivitySlot>
+    <div class="container my-5 text-center">
+      <h2 class="mb-6 text-3xl font-bold text-gray-800">User Reviews</h2>
+      <p class="mb-2 text-gray-600">
+        What our users have to say about our task management solution.
+      </p>
+    </div>
+    <div class="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+      <UAlert
+        v-for="(user, index) in user_reviews"
+        :key="index"
+        :title="user.name"
+        :description="user.description"
+        :avatar="{ src: user.userImage, alt: user.name }"
+      >
+        <template #avatar="{ avatar }">
+          <UAvatar
+            v-bind="avatar"
+            :chip-color="index % 2 === 0 ? 'green' : 'orange'"
+            chip-position="top-left"
+          />
+        </template>
+      </UAlert>
+    </div>
+  </UiKitsUiSlotsProjectActivitySlot>
 </template>
 
 <script setup lang="ts">
