70b09b832c9ef905e0a7e9fe908ccc0698d8c0f9
danieladeabah <daniel.adeabah@amalitech.com>
2025-01-04 14:18:39 +0000

    refactor: clean up sendEmail function for improved readability

--- src/server/utils/sendEmail.ts
+++ src/server/utils/sendEmail.ts
@@ -12,23 +12,21 @@ export const sendEmail = async ({
   // Create a transporter object using Ethereal SMTP
   const transporter = nodemailer.createTransport({
     host: 'smtp.ethereal.email',
-    port: 587, // or 465 if you're using SSL
-    secure: false, // set to true for SSL
+    port: 587,
     auth: {
-      user: process.env.ETHEREAL_EMAIL_USERNAME, // Your Ethereal username
-      pass: process.env.ETHEREAL_EMAIL_PASSWORD // Your Ethereal password
+      user: process.env.ETHEREAL_EMAIL_USERNAME,
+      pass: process.env.ETHEREAL_EMAIL_PASSWORD
     }
   })
 
   const mailOptions = {
-    from: process.env.EMAIL_FROM, // Sender email
-    to, // Receiver email
-    subject, // Subject line
-    html: text // HTML body content
+    from: process.env.EMAIL_FROM,
+    to,
+    subject,
+    html: text
   }
 
   try {
-    // Send the email
     await transporter.sendMail(mailOptions)
     console.log('Email sent successfully!')
   } catch (error) {
